5ca0859f4f8eb20ca07067e7cebf7c704d7b50ef
kaiyuhou <kaiyhou@gmail.com>
2021-11-08 23:30:22 -0600

    speedtest

--- docker_script/speedtest-x/speedtest_x.sh
+++ docker_script/speedtest-x/speedtest_x.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p --restart=on-failure:10 8020:80 --name=speedtest badapple9/speedtest-x
+docker run -d -p 8020:80 --restart=on-failure:10 --name=speedtest badapple9/speedtest-x
