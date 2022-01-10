c727aad865e4676f2571f051a77f7278534c8284
kaiyuhou <kaiyhou@gmail.com>
2022-01-09 18:05:11 -0600

    update gua

--- docker_script/guacamole/start.sh
+++ docker_script/guacamole/start.sh
@@ -4,6 +4,8 @@ mkdir -p ~/guacamole/init
 
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ~/guacamole/init/initdb.sql
 
-docker-compose -f guacamole.yml up -docker
+docker-compose -f guacamole.yml up -d
 
 ### Caddy
+# rewrite * /guacamole{path}
+# default pwd: guacadmin|guacadmin
