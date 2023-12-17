2034ae5ca26903f865b61bb12cf2c36822707de2
kaiyuhou <kaiyhou@gmail.com>
2023-12-17 23:43:39 +0800

    add init setup

---
+++ script/0_init_setup.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+read -p "Hostname: " HOSTNAME
+
+sep_line="========================"
+
+print_sep_line() {
+    echo $sep_line 
+}
+
+add_basic_software() {
+    apt update
+    apt install -y curl htop ufw neofetch nano
+}
+
+show_basic_info() {
+    curl ipinfo.io
+    neofetch
+}
+
+set_host_name() {
+    if [ ! -n "$HOSTNAME" ]; then
+	    echo "Usage: hostname needed"
+	    exit 1
+    fi
+
+    old_hostname=$(hostname)
+    new_hostname=$HOSTNAME
+    echo "previous hostname: $old_hostname"
+    echo "set new hostname: $new_hostname"
+
+    # set hostname file
+    hostname_path="/etc/hostname"
+    sed -i "s/$old_hostname/$new_hostname/" $hostname_path
+    echo $seperate_line
+    echo "[new $hostname_path]"
+    cat $hostname_path
+
+    # set hosts file
+    hosts_path="/etc/hosts"
+    sed -i "s/$old_hostname/$new_hostname/g" $hosts_path
+    echo $seperate_line
+    echo "[new $hosts_path]"
+    cat $hosts_path
+}
+ 
+
+set_utf8() {
+    echo "Set UTF-8"
+    cat /etc/default/locale
+    sed -i 's/LANG=.*/LANG="en_US\.UTF-8"/' /etc/default/locale
+    sed -i 's/LANGUAGE=.*/LANGUAGE="en_US:en"/' /etc/default/locale
+    echo "New config"
+    locale-gen en_US.UTF-8
+    cat /etc/default/locale
+}
+
+enable_bbr() {
+    echo "enable BBR, Requirement: Linux Kernel >= 4.9"
+    uname -r
+
+    echo "==before=="
+    sysctl net.ipv4.tcp_available_congestion_control
+    lsmod | grep bbr
+
+    echo "==after=="
+    echo net.core.default_qdisc=fq >> /etc/sysctl.conf
+    echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
+    sysctl -p
+
+    sysctl net.ipv4.tcp_available_congestion_control
+    lsmod | grep bbr
+}
+
+add_user_mike() {
+    username="mike"
+    shadow_path="/etc/shadow"
+    sudoers_path="/etc/sudoers"
+
+    echo "adduser $username"
+
+    # add user
+    adduser $username --gecos "" --disabled-password
+    adduser $username sudo
+
+    # disable root passwd
+    # echo "disable root password"
+    # usermod -p '!' root
+
+    # set nopasswd in sudo
+    sed -i '$a mike ALL=(ALL) NOPASSWD:ALL' $sudoers_path
+
+    # set ssh config
+    wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/c_set_sshd.sh > c_set_sshd.sh
+    bash ./c_set_sshd.sh
+}
+
+set_ufw() {
+    echo "==before=="
+    ufw status verbose
+    ufw app list
+
+    echo "allow ssh"
+    ufw allow ssh
+
+    echo "enable ufw"
+    echo "y" | ufw enable
+
+    echo "==after=="
+    ufw status verbose
+}
+
+install_docker() {
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+
+    if id -u mike >/dev/null 2>&1; then
+        usermod -aG docker mike
+    fi
+    
+    systemctl enable docker
+    # systemctl start docker
+}
+
+install_caddy() {
+    apt install -y curl debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+    apt update
+    apt install caddy -y
+
+    echo "ufw allow 80 443"
+    ufw allow 80
+    ufw allow 443
+}
+
+reboot_node() {
+    echo $seperate_line
+    read -p "Restart Computer Now? [Y/n]" answer
+
+    if [ "$answer" != "${answer#[Nn]}" ] ; then
+        exit 0
+    fi
+
+    echo "Reboot in 10 Seconds..."
+    sleep 10s
+    echo "Reboot"
+    reboot
+}
+
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+# reset root passwd
+passwd
+
+add_basic_software
+show_basic_info
+set_host_name
+set_utf8
+enable_bbr
+add_user_mike
+set_ufw
+install_docker
+install_caddy
+reboot_node
--- server.sh
+++ server.sh
@@ -11,16 +11,6 @@ if [ ! -d $DIR ];then
 fi
 cd $DIR
 
-#cDIR=`pwd`
-#trap "cl" 2
-#cl () {
-#    echo "..."
-#    echo "clear..."
-#    rm -f $cDIR/$0
-#    echo "OK"
-#    exit 0
-#}
-
 # copy from superbench
 if [ -f /etc/redhat-release ]; then
             release="centos"
@@ -60,28 +50,22 @@ out0 () {
                 fi
         done
 }
-# id=0
-# if [ ! $# -eq 0 ];then
-#     id=$1
-# fi
+
 out0 s "bash <(wget --no-check-certificate -qO- git.io/srvsh)\n"
 out0 r "v 1.0\n"
 while [ 1 -eq 1 ];do
 out0 y "Select a number: \n"
 out1 y "------------------------------------------ \n"
+out1 b " 0. init setup           - Init Setup\n"
 out1 b " 1. bench.sh             - Basic Performance and Network Test\n"
 out1 b " 2. dd.sh (cxthhhhh.com) - DD Instal New OS\n"
-out1 b " 21.dd.sh Ubuntu 18.04   - DD Instal Ubuntu 18.04: cxthhhhh.com\n"
-out1 b " 22.dd.sh win server 2022- DD Instal Win Server 2022: nat.ee\n"
-out1 b " 3. set_hostname_utf8.sh - Set Hostname and UTF-8\n"
-out1 b " 3g. hostname_utf8_bbr.sh- Set Hostname, UTF-8, BBR\n"
+out1 b " 3. hostname_utf8_bbr    - Set Hostname UTF-8, BBR\n"
 out1 b " 4. adduser_mike.sh      - Add User mike\n"
 out1 b " 5. set_sshd.sh          - Disable root and pwd login|Add key for mike\n"
 out1 b " 6. set_ufw.sh           - Enable firewall UFW|Allow port 22\n"
 out1 b " 456. 4 + 5 + 6          - Add User, Set SSH, Set UFW\n"
 out1 b " 7. install_docker.sh    - Install Docker\n"
 out1 b " 76. ipv6nat_for_docker.sh - Enable IPv6 Nat Docker\n"
-#out1 b " 8. igapach.sh           - Install igapach\n"
 out1 b " 9. container-vnc.sh     - Install ubuntu-vnc\n"
 out1 b " f. install_caddy.sh     - Install caddy\n"
 out1 b " 7f. 7 + f docker caddy  - Install docker + caddy\n"
@@ -89,15 +73,22 @@ out1 b " g. enable_bbr.sh        - Enable bbr\n"
 out1 b " h. add_caddy_record.sh  - Add Caddy Record and Reload\n"
 out1 b " s. speedtest-x server   - Docker Speedtest-x port 8020\n"
 out1 b " s3. 3Net Speed Test     - speedtest.sh bash\n"
-out1 r " 0. clear & exit   \n"
+out1 r " q. clear & exit   \n"
 out1 y "------------------------------------------ \n"
 
 
 read id
 case $id in
-    0)
+    q)
         rm -rf ${DIR}
         clear
+        out0 p "Clear Done\n"
+        break
+        ;;
+    0)
+        wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/0_init_setup.sh > init_setup.sh
+        bash ${DIR}/init_setup.sh
+        rm -rf ${DIR}
         out0 p "Clear Done\n"
         break
         ;;
@@ -112,30 +103,10 @@ case $id in
         bash Network-Reinstall-System-Modify.sh -UI_Options
         break
         ;;
-    21)
-        wget --no-check-certificate -qO Network-Reinstall-System-Modify.sh https://raw.githubusercontent.com/kaiyuhou/server/main/dd_os/Network-Reinstall-System-Modify.sh
-        chmod a+x Network-Reinstall-System-Modify.sh
-        bash Network-Reinstall-System-Modify.sh -Ubuntu_18.04
-        break
-        ;;
-    22)
-        wget --no-check-certificate -qO InstallNET.sh  http://d.nat.ee/sh/InstallNET.sh
-        chmod a+x InstallNET.sh
-        apt-get update
-        apt-get install -y xz-utils openssl gawk file grub2
-        bash InstallNET.sh -dd 'http://d.nat.ee/win/lite/winsrv2022-data-x64-cn/winsrv2022-data-x64-cn.vhd.gz'
-        break
-        ;;
     3)
-        wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/a_set_hostname_utf8.sh > a_set_hostname_utf8.sh
+        wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/a_set_hostname_utf8.sh > a_set_hostname_utf8_bbr.sh
         read -p "Hostname: " hostname
-        bash ${DIR}/a_set_hostname_utf8.sh ${hostname}
-        break
-        ;;
-    "3g")
-        wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/ag_set_hostname_utf8_bbr.sh > ag_set_hostname_utf8_bbr.sh
-        read -p "Hostname: " hostname
-        bash ${DIR}/ag_set_hostname_utf8_bbr.sh ${hostname}
+        bash ${DIR}/a_set_hostname_utf8_bbr.sh ${hostname}
         break
         ;;
     4)
@@ -174,11 +145,6 @@ case $id in
         bash ${DIR}/e6_enable_docker_ipv6nat.sh
         break
         ;;
-#    8)
-#        wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/script/x_install_igapach.sh > ${HOME}/x_install_igapach.sh
-#        bash ${HOME}/x_install_igapach.sh
-#        break
-#        ;;
     9)
         wget --no-check-certificate -qO- https://raw.githubusercontent.com/kaiyuhou/server/main/docker_script/ubuntu-vnc/start-ubuntu-vnc.sh > start-ubuntu-vnc.sh
         read -p "Password: " password
@@ -219,26 +185,6 @@ case $id in
         break
         ;;
 
-#    12)
-#    10|16)
-#        if [ $release == "centos" ]; then
-#            yum install epel-release -y
-#            yum install sysbench -y
-#            sysbench cpu run
-#        else
-#            apt-get install sysbench
-#            sysbench cpu run
-#        fi
-#        echo "91yuntest:请选择需要测试的项目序号(以空格分开)"
-#        echo "1.io 2.bandwidth 3.chinabw 4.download 5.traceroute 6.backtraceroute 7.allping 8.gotoping 9.benchtest"
-#        arr=("io," "bandwidth," "chinabw," "download," "traceroute," "backtraceroute," "allping," "gotoping," "benchtest,")
-#        read -r m
-#        st=""
-#        for i in $m ; do
-#            st=${st}${arr[$(($i-1))]}
-#        done
-#        break
-#        ;;
     *)
         clear
         out0 p "Please Reselect\n"
