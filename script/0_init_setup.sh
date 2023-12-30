06a0b95024ff8befecf9fb97e3be6fca0b174f51
kaiyuhou <kaiyhou@gmail.com>
2023-12-30 23:31:57 +0800

    remove before bbr check

--- script/0_init_setup.sh
+++ script/0_init_setup.sh
@@ -59,11 +59,6 @@ enable_bbr() {
     echo "enable BBR, Requirement: Linux Kernel >= 4.9"
     uname -r
 
-    echo "==before=="
-    sysctl net.ipv4.tcp_available_congestion_control
-    lsmod | grep bbr
-
-    echo "==after=="
     echo net.core.default_qdisc=fq >> /etc/sysctl.conf
     echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
     sysctl -p
