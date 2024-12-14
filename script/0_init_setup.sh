d4bfd78747a45e94a5f418b0d8611ed1d78fe154
kaiyu hou <kaiyhou@gmail.com>
2024-12-14 19:40:43 +0800

    fix: init setup

--- script/0_init_setup.sh
+++ script/0_init_setup.sh
@@ -10,7 +10,7 @@ print_sep_line() {
 
 add_basic_software() {
     apt update
-    apt install -y curl htop ufw neofetch nano locale
+    apt install -y curl htop ufw neofetch nano locales
 }
 
 show_basic_info() {
